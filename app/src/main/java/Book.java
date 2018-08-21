6ccbda923e87a40e1803c77d9d8d2a1cbe552008
Efana <usuaefana@gmail.com>
2018-08-21 21:35:31 +0100

    Renamed MainActivity to BookActivity, created new classes Book and BookUtils.

---
+++ app/src/main/java/Book.java
@@ -0,0 +1,9 @@
+public class Book {
+
+    /** Title of the book */
+    private String mTitle;
+
+    public Book(String title){
+        mTitle = title;
+    }
+}
--- app/src/main/java/com/example/android/googlebooklistingapp/BookActivity.java
+++ app/src/main/java/com/example/android/googlebooklistingapp/BookActivity.java
@@ -5,6 +5,12 @@ import android.os.Bundle;
 
 public class BookActivity extends AppCompatActivity {
 
+    public static final String LOG_TAG = BookActivity.class.getName();
+
+    /** URL for books data from the Googlebooks dataset */
+    private static final String GOOGLEBOOKS_REQUEST_URL =
+            "https://www.googleapis.com/books/v1/volumes?q=cakes&maxResults=2";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
