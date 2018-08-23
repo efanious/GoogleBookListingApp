2b7c3a158ca3d801f62ec1fdf8c6975b3f4f22fe
Efana <usuaefana@gmail.com>
2018-08-23 01:28:46 +0100

    Modifying networking and other changes

--- app/src/main/java/com/example/android/googlebooklistingapp/BookActivity.java
+++ app/src/main/java/com/example/android/googlebooklistingapp/BookActivity.java
@@ -1,12 +1,16 @@
 package com.example.android.googlebooklistingapp;
 
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ListView;
 
 import java.util.ArrayList;
+import java.util.List;
 
-public class BookActivity extends AppCompatActivity {
+public class BookActivity extends AppCompatActivity
+        implements LoaderManager.LoaderCallbacks<List<Book>>{
 
     /** Adapter for the list of books */
     private BookAdapter mAdapter;
@@ -16,6 +20,8 @@ public class BookActivity extends AppCompatActivity {
     /** URL for books data from the Googlebooks dataset */
     private static final String GOOGLEBOOKS_REQUEST_URL =
             "https://www.googleapis.com/books/v1/volumes?q=cakes&maxResults=2";
+
+    private static final int EARTHQUAKE_LOADER_ID = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +37,35 @@ public class BookActivity extends AppCompatActivity {
         // Set the adapter on the {@link ListView}
         // so the list can be populated in the user interface
         bookListView.setAdapter(mAdapter);
+
+        getSupportLoaderManager().initLoader(EARTHQUAKE_LOADER_ID, null, this);
+
+    }
+
+    @Override
+    public Loader<List<Book>> onCreateLoader(int id, Bundle args) {
+
+        return new BookLoader(BookActivity.this,GOOGLEBOOKS_REQUEST_URL);
+    }
+
+    @Override
+    public void onLoadFinished(Loader<List<Book>> loader, List<Book> data) {
+
+        // Clear the adapter of previous earthquake data
+        mAdapter.clear();
+
+        // If there is a valid list of {@link Book}s, then add them to the adapter's
+        // data set. This will trigger the ListView to update.
+        if (data != null && !data.isEmpty()) {
+
+            mAdapter.addAll(data);
+        }
+    }
+
+    @Override
+    public void onLoaderReset(Loader<List<Book>> loader) {
+        // Loader reset, so we can clear out our existing data.
+        mAdapter.clear();
 
     }
 }
